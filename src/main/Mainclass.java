b1399098c84a956745cbecea6c77a5db87108a33
Michael Loukeris <loukerismichalis@gmail.com>
2016-12-06 23:53:50 +0200

    edited class description

--- src/main/Mainclass.java
+++ src/main/Mainclass.java
@@ -1,10 +1,8 @@
 
+
 /**********************Main class**************************
 ***********************************************************
 **1) Contains Main Method.*********************************
-**2) Creates Threads.**************************************
-**3) Defines How many Layers Crawler will scan.************
-**4) Contains Run() Method.********************************
-**5) Concatenates All Thread Lists to One Final List.******
+**2) Defines How many Layers Crawler will scan.************
+**3) Concatenates All Thread Lists to One Final List.******
 ***********************************************************/
-
