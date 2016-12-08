7fb7a2d94014688215694aa2562cea2a7d68841b
Michael Loukeris <loukerismichalis@gmail.com>
2016-12-08 18:14:37 +0200

    converted variables to private, changed some stuff , optimized code

--- src/main/Mainclass.java
+++ src/main/Mainclass.java
@@ -1,67 +1,51 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Mainclass {
+public class Mainclass extends Thread {
 
+	private static int file_number; // number of files is a directory
+	private static int run_times = 1;
 	private static int layers = 1; // Default layers is 1
-	private static char answer;
-	private static String email;
+	private static char answer; // If user desires or not to receive a result
+								// email
+	private static String email; // User email
 	private static String path; // Path of output
 	private static String t1name; // thread 1 name
-	private static String t2name; // thread 2 name+
+	private static String t2name; // thread 2 name
 	private static String t3name; // thread 3 name
 	private static String link1 = "https://github.com/milouk";
 	private static String link2 = "https://review.cyanogenmod.org/";
 	private static String link3 = "https://www.cmxlog.com/13";
 
 	// thread 1 result list
-	public static ArrayList<String> thread1_list = new ArrayList<String>();
-	public static Set<String> thread1_set = new LinkedHashSet<String>();
+	private static ArrayList<String> thread1_list = new ArrayList<String>();
+	private static Set<String> thread1_set = new LinkedHashSet<String>();
 
 	// thread 2 result list
-	public static ArrayList<String> thread2_list = new ArrayList<String>();
-	public static Set<String> thread2_set = new LinkedHashSet<String>();
+	private static ArrayList<String> thread2_list = new ArrayList<String>();
+	private static Set<String> thread2_set = new LinkedHashSet<String>();
 
 	// thread 3 result list
-	public static ArrayList<String> thread3_list = new ArrayList<String>();
-	public static Set<String> thread3_set = new LinkedHashSet<String>();
+	private static ArrayList<String> thread3_list = new ArrayList<String>();
+	private static Set<String> thread3_set = new LinkedHashSet<String>();
 
 	// Final Result List
-	public static ArrayList<String> finalist = new ArrayList<String>();
+	private static ArrayList<String> finalist = new ArrayList<String>();
 
 	private static Scanner input;
 	private static Scanner inputpath;
 	private static Scanner yorn;
 	private static Scanner mail;
 
-	//Gets for thread names
-	public String getT1name() {
-		return t1name;
-	}
-	public String getT2name() {
-		return t2name;
-	}
-	public String getT3name() {
-		return t3name;
-	}
+	public static void main(String[] args) {
 
-	//Gets for links
-	public String getLink1() {
-		return link1;
-	}
-	public String getLink2() {
-		return link2;
-	}
-	public String getLink3() {
-		return link3;
-	}
-
-
-    public static void main(final String[] args) throws Exception {
-
+		Mainclass thread = new Mainclass();
 		input = new Scanner(System.in);
 		inputpath = new Scanner(System.in);
 		yorn = new Scanner(System.in);
@@ -84,3 +68,27 @@ public class Mainclass {
 			email = mail.nextLine();
 
 		}
+
+		System.out.printf("%s",
+				"Enter How Many Layers You Would Like The Crawler To Complete (Default is 1 Layer)  : ");
+
+		layers = input.nextInt();
+
+		// if user enters 0 for layers number, Program terminates
+		if (layers != 0) {
+
+			System.out.println("\n*****************************************************");
+			System.out.println("*Please wait, this might take a while to complete...*");
+			System.out.println("*****************************************************\n\n");
+
+			thread.start();
+
+		} else {
+
+			System.out.println("\n************");
+			System.out.println("* Finished *");
+			System.out.println("************\n\n");
+		}
+
+	}
+
