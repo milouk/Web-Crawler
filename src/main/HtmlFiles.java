713902edc8460003c027d0d69673ee1b8e14c823
Ioanna <Ioanna>
2016-12-09 15:13:50 +0200

    createFile method, create a URL, make a connection and fill the array list

--- src/main/HtmlFiles.java
+++ src/main/HtmlFiles.java
@@ -1,9 +1,24 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.net.*;
 
 public class HtmlFiles {
 
 	public static ArrayList<String> lines = new ArrayList<String>();
 
+	public static void createFile(String link, String path, int index) throws IOException {
+
+		URL url = new URL(link);
+		URLConnection connection = url.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null) {
+
+			lines.add(inputLine);
+		}
+		in.close();
+	}
 }
+
 
