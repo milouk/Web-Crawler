2bcedf8c43382cab085093ed87812c1fadcab714
Theonimfi <theon.anog@gmail.com>
2016-12-11 19:25:32 +0200

    Insert data -position, urls, pathx.html- in sql server and add SQLException

--- src/main/DatabaseConnection.java
+++ src/main/DatabaseConnection.java
@@ -7,7 +7,19 @@ public static Connection dbcon;
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			dbcon = DriverManager.getConnection("jdbc:sqlserver://serverName:1025;databaseName=complet;user=team3;password=2016;");
+			Statement stmt = dbcon.createStatement();
+			String pathHtml;
+			int position;
+		    for(int i = 0; i < array.size(); i++) {
+		        position = i;
+				pathHtml = path + "/" + Integer.toString(i) + ".html";
+				stmt.executeUpdate("INSERT INTO DatabaseOfURLs VALUES('" + position + "','" + array.get(i) + "','" + pathHtml + "')");
+            }
+    		stmt.close();
 		    dbcon.close();
+
+        } catch (SQLException sqle) {
+					System.err.println(sqle);
 		} catch (Exception e) {
 			System.err.println(e);
 		}
