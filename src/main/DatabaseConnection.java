8b3d793eb65866ebe08b726c7f7557b08f1fcaf1
klairh <klairh>
2016-12-11 16:04:18 +0200

    Create connection with Database in java

--- src/main/DatabaseConnection.java
+++ src/main/DatabaseConnection.java
@@ -1,3 +1,15 @@
-public class DatabaseConnection {
+import java.sql.*;
+import java.util.ArrayList;
 
+public class DatabaseConnection {
+public static Connection dbcon;
+	public static void InsertData(ArrayList<String> array, String path) {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			dbcon = DriverManager.getConnection("jdbc:sqlserver://serverName:1025;databaseName=complet;user=team3;password=2016;");
+		    dbcon.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
 }
