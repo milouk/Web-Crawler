ec41a8bc95d671edd1448c95caba96193492fa09
klairh <klairh>
2016-12-14 00:56:57 +0200

    read an image from Url

--- src/main/MediaCheck.java
+++ src/main/MediaCheck.java
@@ -1,10 +1,22 @@
+package com.complet;
+
 import java.awt.Image;
 import java.net.URL;
+import javax.imageio.ImageIO;
 
 public class MediaCheck {
 
     public static void media(URL url) {
 
 	    Image image = null;
+        try {
+
+            image = ImageIO.read(url);
+
+        } catch (Exception e) {
+
+            System.err.println(e);
+
+        }
     }
  }
