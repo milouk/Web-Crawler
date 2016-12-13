7eae7bf22e3ddbfeae58710445e125caf1c56228
Theonimfi <theon.anog@gmail.com>
2016-12-13 18:55:50 +0200

    Create a method which contains URL as argument and create an Image variable

--- src/main/MediaCheck.java
+++ src/main/MediaCheck.java
@@ -1,2 +1,10 @@
- public class MediaCheck {
+import java.awt.Image;
+import java.net.URL;
+
+public class MediaCheck {
+
+    public static void media(URL url) {
+
+	    Image image = null;
+    }
  }
