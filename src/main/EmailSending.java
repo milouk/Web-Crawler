2c209c6618f8cd9f89b21411f653568406533d9c
SanetsisPanagiotis <sanetsis.panagiotis@gmail.com>
2016-12-14 21:37:57 +0200

    Initialized important imports that will be a part of the EmailSending class/

--- src/main/EmailSending.java
+++ src/main/EmailSending.java
@@ -1,1 +1,14 @@
+package com.complet;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSending {
+
+}
 
