97c2fcc06c9382ed59e5d36a86df065c104042a8
unknown <arguro_s@outlook.com>
2016-12-15 17:13:03 +0200

    Added classes to be used and creation of HttpURLConnection object

--- src/main/ServerResponse.java
+++ src/main/ServerResponse.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.net.*;
+
 public class ServerResponse {
+
+	public static void response(URL url) throws IOException {
+
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	}
 
 }
