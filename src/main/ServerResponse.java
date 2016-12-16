ec33ed8a50d966bd54c8622eebce9b6aa2667e43
Theonimfi <theon.anog@gmail.com>
2016-12-16 12:14:22 +0200

    Call the connect method -of HttpUrlConnection Class- to initiate the connection, catch exceptions, define the package

--- src/main/ServerResponse.java
+++ src/main/ServerResponse.java
@@ -1,3 +1,5 @@
+package com.complet;
+
 import java.io.IOException;
 import java.net.*;
 
@@ -6,6 +8,16 @@ public class ServerResponse {
 	public static void response(URL url) throws IOException {
 
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+		try {
+
+			connection.connect();
+
+		} catch (Exception exp) {
+
+		    System.err.println(exp.getMessage());
+
+		}
 	}
 
 }
