84a4aa08d6b8f6b8f828e6bb77d426c90333b5bd
SanetsisPanagiotis <sanetsis.panagiotis@gmail.com>
2016-12-17 11:11:30 +0200

    After a long effort i managed to find the necessary mail trasnfer protocol(S.M.T.P.) port for our class in order to communicate with Google's mail services

--- src/main/EmailSending.java
+++ src/main/EmailSending.java
@@ -10,5 +10,25 @@ import javax.mail.internet.MimeMessage;
 
 public class EmailSending {
 
+
+	public static void email(String email) {
+
+		// Destination Email
+		String to = email;
+
+		String host = "pop.gmail.com";
+
+		// Get system properties
+		Properties properties = System.getProperties();
+
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");
+
+		properties.put("mail.smtp.starttls.enable", "true");
+
+		properties.setProperty("mail.smtp.host", host);
+
+	}
+
 }
 
