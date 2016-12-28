0e93076283733fbd87eab91584d0b9849ce48bff
klairh <klairh>
2016-12-28 21:15:06 +0200

    Create a separate method to connect with Database

--- src/main/DatabaseConnection.java
+++ src/main/DatabaseConnection.java
@@ -1,16 +1,33 @@
-import java.sql.*;
+package com.complet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DatabaseConnection {
-public static Connection dbcon;
-public static Statement stmt;
-	public static void InsertData(ArrayList<String> array, String path) {
+    public static Connection dbcon;
+    public static Statement stmt;
+    public static void openDatabaseConnection() {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			dbcon = DriverManager.getConnection("jdbc:sqlserver://serverName:1025;databaseName=complet;user=team3;password=2016;");
-			stmt = dbcon.createStatement();
+			dbcon = DriverManager.getConnection(
+					"jdbc:sqlserver://".concat(GetCredentials.getSqlServerPath()).concat(";databaseName=")
+					.concat(GetCredentials.getDbName()).concat(";user=").concat(GetCredentials.getDbUser())
+					.concat(";password=").concat(GetCredentials.getDbPassword()).concat(";"));
+		    stmt = dbcon.createStatement();
+		 } catch (SQLException sqle) {
+			System.err.println(sqle);
+		 } catch (Exception e) {
+			System.err.println(e);
+		 }
+	}
+	public static void InsertData(ArrayList<String> array, String path) {
+		try {
 			String pathHtml;
 			int position;
+			openDatabaseConnection();
 		    for(int i = 0; i < array.size(); i++) {
 		        position = i;
 				pathHtml = path + "/" + Integer.toString(i) + ".html";
@@ -20,7 +37,7 @@ public static Statement stmt;
 		    dbcon.close();
 
         } catch (SQLException sqle) {
-					System.err.println(sqle);
+			System.err.println(sqle);
 		} catch (Exception e) {
 			System.err.println(e);
 		} finally {
