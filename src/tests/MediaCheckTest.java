62febdfd00d56d2d30dcb73ed0b5945a4aab1689
Ioanna <Ioanna>
2016-12-30 20:52:25 +0200

    Make a timeout if the test is running for more than 10s

--- src/tests/MediaCheckTest.java
+++ src/tests/MediaCheckTest.java
@@ -1,30 +1,34 @@
+package com.complet;
+
 import static org.junit.Assert.*;
+
 import org.junit.Test;
-import java.net.MalformedURLException;
-import java.net.URL;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
-//import org.junit.TestTimedOutException;
-//import org.junit.rules.Timeout;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+import com.complet.MediaCheck;
 
 import java.lang.InterruptedException;
 
 public class MediaCheckTest {
-	
+
 
 	@Rule
-	//public Timeout globalTimeout = Timeout.seconds(20);
+
 	public ExpectedException excepion = ExpectedException.none();
 
 
-	@Test//(timeout=100) 
+	@Test (timeout=10000)
 	public void testMedia()  throws MalformedURLException, InterruptedException{
-		
+
 		URL url = new URL("https://tctechcrunch2011.files.wordpress.com/2013/05/house-of-cards-kevin-spacey.jpg");
 		assertFalse(MediaCheck.media(url));
-		//Thread.sleep(200);
-	 
-	}
-	
-}
 
+
+	}
+
+}
