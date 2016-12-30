708d66f598479cb92cc1cd15b8a1cb1fc6863097
SanetsisPanagiotis <sanetsis.panagiotis@gmail.com>
2016-12-30 11:20:46 +0200

    Added JavaDOCumention for EmailSending class

--- src/main/EmailSending.java
+++ src/main/EmailSending.java
@@ -8,8 +8,46 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+/**
+ * <h1>Send an Email - EmailSending.java</h1> The EmailSending Class is able to send an email using
+ * the simple mail transfer protocol (S.M.T.P.)
+ * <p>
+ * <b>Note: </b>Must get a valid E-mail in order to execute the process,
+ * otherwise, it will not work.
+ * <p>
+ * @author Complet
+ * @version 5.0
+ * @since 2016-12-29
+ *
+ */
+
 public class EmailSending {
 
+	/**
+	 * <p>
+	 * This is the only method of the class. Using the correct properties, it is
+	 * able to connect with the google mail Service and send the E-mail
+	 * successfully.
+	 * <p>
+	 * Google mail service demands a valid account. And therefor, we use
+	 * Credentials that draw the account settings from the EmailCredentials.txt
+	 * document.
+	 * <p>
+	 * <b>The content of the E-mail:</b><br>
+	 * <li>Number of Layers used <br>
+	 * <li>Url's that were crawled <br>
+	 * <li>Results (Number of Links) <br>
+	 * <li>Number of Run_times (Our program can literally run forever, updating
+	 * the database) <br>
+	 * <li>Date and Time the program started (for the given Run_time)<br>
+	 * <li>Date and TIme the program "ended" (for the given Run_time)
+	 * <p>
+	 *
+	 * @param email The recipient mail.
+	 * @return Nothing.
+	 * @exception Exception It varies from time to time based on the Google Mail Service. Cannot predict exceptions based on their Services.
+	 *
+	 */
 
 	public static void email(String email) {
 
