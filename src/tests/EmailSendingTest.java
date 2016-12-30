2e46dbfb952c4baf2fdf54782c58fc8217a251bb
Ioanna <Ioanna>
2016-12-30 20:57:13 +0200

    Move the tests at com.complet package

--- src/tests/EmailSendingTest.java
+++ src/tests/EmailSendingTest.java
@@ -1,3 +1,5 @@
+package com.complet;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -20,56 +22,56 @@ import java.util.Properties;
 
 
 public class EmailSendingTest {
-	
+
 
 	private EmailSending mailSender;
-	
+
 	Authenticator PasswordAuthentication;
-	
+
 	Properties props =System.getProperties();
- 
-	
+
+
 	@Before
 	 public void setUp() {
 	  mailSender = new EmailSending();
-	  
+
 	  props.put("mail.smtp.auth", "false");
 	  props.put("mail.smtp.starttls.enable", "true");
 	  props.put("mail.smtp.host", "host");
 	  props.put("mail.smtp.port", "25");
 
 	 }
-	
-	 @Test 
+
+	 @Test
 	 public void testEmail()  throws MessagingException, IOException {
 
 	  String subject = "Test1";
 	  String body = "Test Message1";
 	  EmailSending.email("joannaschiza13@gmail.com");
 	  try {
-		  
-		
+
+
 		Session session = Session.getInstance(props);
 
           try {
         	  Store store = session.getStore("smtp");
-        	  
+
         	  store.connect("gmail.com", "joannaschiza13", "password");
         	  Folder folder = store.getFolder("INBOX");
         	  folder.open(Folder.READ_ONLY);
 		      Message[] msg = folder.getMessages();
-		  
+
 		      assertTrue(msg.length == 1);
 		      assertEquals(subject, msg[0].getSubject());
 		      assertEquals(body, msg[0].getContent());
 		      assertNotNull(body);
 		      folder.close(true);
 		      store.close();
-		      
+
           } catch (NoSuchProviderException nspe) {
         	  System.err.println(nspe.getMessage());
           }
-          
+
 	   } catch (SecurityException se) {
 		  System.err.println("Could not write to file " + se.getMessage());
 	  }
--- src/tests/TestRunner.java
+++ src/tests/TestRunner.java
@@ -1,19 +1,21 @@
+package com.complet;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
- 
+
 
 public class TestRunner {
- 
+
 	public static void main(String[] args) {
-		
+
 		Result result = JUnitCore.runClasses(MediaCheckTest.class);
 		for (Failure fail : result.getFailures()) {
 			System.out.println(fail.toString());
 		}
 		if (result.wasSuccessful()) {
 			System.out.println("Test finished successfully...");
-		
+
 		}else{
 		    System.out.println("The test failed..");
 		}
