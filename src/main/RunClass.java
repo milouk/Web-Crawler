b2effae18fcf45baa41d9e4b8c2d77334d71b7c9
Michael <loukerismichalis@gmail.com>
2017-01-02 19:10:45 +0200

    added java documentation

--- src/main/RunClass.java
+++ src/main/RunClass.java
@@ -1,16 +1,35 @@
 package com.complet;
 
+/**
+ * <h1>Run Class - RunClass.java</h1> The Run Class is a class that contains
+ * what the threads will do. It also Ensures that all thread are finished before
+ * proceeding to next run.
+ *
+ * @author Complet
+ * @version 5.0
+ * @since 2017-01-02
+ */
+
 public class RunClass extends Thread {
 
-	// These are going to help the RobotTags.checkAccess method to work properly
-	// In general, they tell if something is indexable or followable by true or
-	// false
 	public static boolean robotFollow_thread1 = true;
 	public static boolean robotFollow_thread2 = true;
 	public static boolean robotFollow_thread3 = true;
 	public static boolean robotIndex_thread1 = true;
 	public static boolean robotIndex_thread2 = true;
 	public static boolean robotIndex_thread3 = true;
+
+	/**
+	 * <p>
+	 * begin is the first method of this class. It creates 3 threads and ensures
+	 * these threads are finished. When threads are finished it empties finalist
+	 * in order to get it ready for next run.
+	 * <p>
+	 *
+	 * @exception InterruptedException
+	 *                which occurs if one of the threads unexpectedly is
+	 *                interrupted.
+	 */
 
 	public static void begin() {
 
@@ -35,10 +54,25 @@ public class RunClass extends Thread {
 			m3.join();
 
 		} catch (InterruptedException e) {
+			System.err.println("Thread Was Unexcpectedly Interrupted! ");
 		}
 
+		// Empties Finalist for next run.
 		Mainclass.getFinalist().removeAll(Mainclass.getFinalist());
+
 	}
+
+	/**
+	 * <p>
+	 * Run Method. Calls the start method from LinkRetrieve class in order the
+	 * extraction to begin. Simultaneously it filters links containing meta
+	 * robot tags. This is also where layers are controlled.
+	 * <p>
+	 *
+	 * @exception Exception
+	 *                which occurs when something in the Robot Tags Class Goes
+	 *                Wrong.
+	 */
 
 	@Override
 	public final void run() {
@@ -51,24 +85,14 @@ public class RunClass extends Thread {
 				RobotTags.checkAccess(Mainclass.getLink1());
 
 			} catch (Exception e) {
+
 				System.err.println(e);
+
 			}
 
 			if (robotFollow_thread1) {
 				// initial run thread 1 (link 1)
 				LinkRetrieve.start(Mainclass.getLink1());
-
-				// Add All Elements In The List To The Set
-				Mainclass.getThread1_set().addAll(Mainclass.getThread1_list());
-
-				// Empty List
-				Mainclass.getThread1_list().removeAll(Mainclass.getThread1_list());
-
-				// Add Back To The List Only Unique Elements
-				Mainclass.getThread1_list().addAll(Mainclass.getThread1_set());
-
-				// Empty Set
-				Mainclass.getThread1_set().removeAll(Mainclass.getThread1_set());
 			}
 
 		} else if (currentThread().getName().equals(Mainclass.getT2name())) {
@@ -79,26 +103,14 @@ public class RunClass extends Thread {
 				RobotTags.checkAccess(Mainclass.getLink2());
 
 			} catch (Exception e) {
+
 				System.err.println(e);
 			}
 
 			if (robotFollow_thread2) {
 				// initial run thread 2 ( link 2)
 				LinkRetrieve.start(Mainclass.getLink2());
-
-				// Add All Elements In The List To The Set
-				Mainclass.getThread2_set().addAll(Mainclass.getThread2_list());
-
-				// Empty List
-				Mainclass.getThread2_list().removeAll(Mainclass.getThread2_list());
-
-				// Add Back To The List Only Unique Elements
-				Mainclass.getThread2_list().addAll(Mainclass.getThread2_set());
-
-				// Empty Set
-				Mainclass.getThread2_set().removeAll(Mainclass.getThread2_set());
 			}
-
 		} else if (currentThread().getName().equals(Mainclass.getT3name())) {
 
 			// Check if robot tags exists and allows to follow links
@@ -107,25 +119,16 @@ public class RunClass extends Thread {
 				RobotTags.checkAccess(Mainclass.getLink3());
 
 			} catch (Exception e) {
+
 				System.err.println(e);
+
 			}
 
 			if (robotFollow_thread3) {
 				// initial run thread 3 (link 3)
 				LinkRetrieve.start(Mainclass.getLink3());
-
-				// Add All Elements In The List To The Set
-				Mainclass.getThread3_set().addAll(Mainclass.getThread3_list());
-
-				// Empty List
-				Mainclass.getThread3_list().removeAll(Mainclass.getThread3_list());
-
-				// Add Back To The List Only Unique Elements
-				Mainclass.getThread3_list().addAll(Mainclass.getThread3_set());
-
-				// Empty Set
-				Mainclass.getThread3_set().removeAll(Mainclass.getThread3_set());
 			}
+
 		}
 
 		/*
@@ -145,23 +148,12 @@ public class RunClass extends Thread {
 				// call start method for every link in list
 				while (starting_point < ending_point) {
 
-					// Check if robot tags allows to follow links
+					// Check if robot tags exists and allows to follow links
 
 					if (RobotTags.thread1_mFollow.get(starting_point)) {
 
 						LinkRetrieve.start(Mainclass.getThread1_list().get(starting_point));
 
-						// Add All Elements In The List To The Set
-						Mainclass.getThread1_set().addAll(Mainclass.getThread1_list());
-
-						// Empty List
-						Mainclass.getThread1_list().removeAll(Mainclass.getThread1_list());
-
-						// Add Back To The List Only Unique Elements
-						Mainclass.getThread1_list().addAll(Mainclass.getThread1_set());
-
-						// Empty Set
-						Mainclass.getThread1_set().removeAll(Mainclass.getThread1_set());
 					}
 
 					starting_point++;
@@ -172,6 +164,7 @@ public class RunClass extends Thread {
 					ending_point = Mainclass.getThread1_list().size();
 
 				} else {
+
 					return;
 				}
 
@@ -194,23 +187,12 @@ public class RunClass extends Thread {
 				// call start method for every link in list
 				while (starting_point < ending_point) {
 
-					// Check if robot tags allows to follow links
+					// Check if robot tags exists and allows to follow links
 
 					if (RobotTags.thread2_mFollow.get(starting_point)) {
 
 						LinkRetrieve.start(Mainclass.getThread2_list().get(starting_point));
 
-						// Add All Elements In The List To The Set
-						Mainclass.getThread2_set().addAll(Mainclass.getThread2_list());
-
-						// Empty List
-						Mainclass.getThread2_list().removeAll(Mainclass.getThread2_list());
-
-						// Add Back To The List Only Unique Elements
-						Mainclass.getThread2_list().addAll(Mainclass.getThread2_set());
-
-						// Empty Set
-						Mainclass.getThread2_set().removeAll(Mainclass.getThread2_set());
 					}
 
 					starting_point++;
@@ -241,23 +223,11 @@ public class RunClass extends Thread {
 				// call start method for every link in list
 				while (starting_point < ending_point) {
 
-					// Check if robot tags allows to follow links
+					// Check if robot tags exists and allows to follow links
 
 					if (RobotTags.thread3_mFollow.get(starting_point)) {
 
 						LinkRetrieve.start(Mainclass.getThread3_list().get(starting_point));
-
-						// Add All Elements In The List To The Set
-						Mainclass.getThread3_set().addAll(Mainclass.getThread3_list());
-
-						// Empty List
-						Mainclass.getThread3_list().removeAll(Mainclass.getThread3_list());
-
-						// Add Back To The List Only Unique Elements
-						Mainclass.getThread3_list().addAll(Mainclass.getThread3_set());
-
-						// Empty Set
-						Mainclass.getThread3_set().removeAll(Mainclass.getThread3_set());
 					}
 
 					starting_point++;
@@ -268,9 +238,12 @@ public class RunClass extends Thread {
 					ending_point = Mainclass.getThread3_list().size();
 
 				} else {
+
 					return;
 				}
+
 			}
 		}
+
 	}
 }
