e08cd72844add87e5ab9605fe81bb1465b508cd9
Michael <loukerismichalis@gmail.com>
2017-01-02 16:12:26 +0200

    improved java documentation

--- src/main/DatabaseConnection.java
+++ src/main/DatabaseConnection.java
@@ -4,15 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 /**
- * This class consists exclusively of static methods that
- * operate on a database. It contains methods that connect
- * to the database, disconnect from it and update it.
+ * <h1>DataBase Connection Class - DatabaseConnection.java</h1> This class
+ * consists exclusively of static methods that operate on a database. It
+ * contains methods that connect to the database, disconnect from it and update
+ * it.
  *
- * The methods of this class all throw a SQLException in
- * case something goes wrong between the database-java communication.
+ * @author Complet
+ * @version 5.0
+ * @since 2017-01-02
  */
 
 public class DatabaseConnection {
@@ -22,24 +23,34 @@ public class DatabaseConnection {
 	/**
 	 * Creates a connection to a database.
 	 *
-	 * @throws Exception if the registration to the JDBC driver fails.
-	 * @throws SQLException if the connection to the database fails.
+	 * @throws Exception
+	 *             if the registration to the JDBC driver fails.
+	 * @throws SQLException
+	 *             if the connection to the database fails.
 	 */
 
 	public static void openDatabaseConnection() {
 		try {
-			GetCredentials.dbCredentials("path DataBase-Credentials.txt"); //Calls the dbCredentials method
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); //Registers to the JDBC driver
+			GetCredentials.dbCredentials("path DataBase-Credentials.txt"); // Calls
+																			// the
+																			// dbCredentials
+																			// method
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Registers
+																			// to
+																			// the
+																			// JDBC
+																			// driver
 			dbcon = DriverManager.getConnection(
 					"jdbc:sqlserver://".concat(GetCredentials.getSqlServerPath()).concat(";databaseName=")
 							.concat(GetCredentials.getDbName()).concat(";user=").concat(GetCredentials.getDbUser())
 							.concat(";password=").concat(GetCredentials.getDbPassword()).concat(";"));
-			stmt = dbcon.createStatement(); //Creates a statement that represents a SQL statement
+			stmt = dbcon.createStatement(); // Creates a statement that
+											// represents a SQL statement
 		} catch (SQLException sqle) {
-								//Handles errors for JDBC
+			// Handles errors for JDBC
 			System.err.println(sqle);
 		} catch (Exception e) {
-								//Handles errors for Class.forName
+			// Handles errors for Class.forName
 			System.err.println(e);
 		}
 	}
@@ -48,21 +59,23 @@ public class DatabaseConnection {
 	 * Closes the open connection to the database.
 	 *
 	 * @throws Exception
-	 * @throws SQLException if there is a problem disconnecting from the database.
+	 * @throws SQLException
+	 *             if there is a problem disconnecting from the database.
 	 */
 
 	public static void closeDatabaseConnection() {
 		try {
-			openDatabaseConnection(); //Calls the openDatabaseConnection method to ensure there is an open connection
-			stmt.close(); //Closes the Statement object
-			dbcon.close(); //Closes the Connection object
+			openDatabaseConnection(); // Calls the openDatabaseConnection method
+										// to ensure there is an open connection
+			stmt.close(); // Closes the Statement object
+			dbcon.close(); // Closes the Connection object
 		} catch (SQLException sqle) {
-								//Handles errors for JDBC
+			// Handles errors for JDBC
 			System.err.println(sqle);
 		} catch (Exception e) {
 			System.err.println(e);
 		} finally {
-							//Makes sure there is no leak in resources
+			// Makes sure there is no leak in resources
 			try {
 				if (stmt != null) {
 					stmt.close();
@@ -81,24 +94,33 @@ public class DatabaseConnection {
 	}
 
 	/**
-	 * Inserts data into the database.
-	 * The array argument specifies an ArrayList that contains URLs.
-	 * The path argument is a specifier for a file that contains HTML text.
+	 * Inserts data into the database. The array argument specifies an ArrayList
+	 * that contains URLs. The path argument is a specifier for a file that
+	 * contains HTML text.
 	 *
-	 * @array an ArrayList that contains the URLs to be inserted in the database in form of Strings.
-     * @param path a path that points to the file where the HTML text of the links is saved.
-     * @throws SQLException if an error occurs during the data insertion.
-     */
+	 * @array an ArrayList that contains the URLs to be inserted in the database
+	 *        in form of Strings.
+	 * @param path
+	 *            a path that points to the file where the HTML text of the
+	 *            links is saved.
+	 * @throws SQLException
+	 *             if an error occurs during the data insertion.
+	 */
 
 	public static void InsertData(String links, String path) {
 		try {
 			String pathHtml;
 
-			openDatabaseConnection(); //Creates a connection to the database
-			pathHtml = path + "/" + Integer.toString(Mainclass.getPosition()) + ".html"; //Specifies the path of each HTML
+			openDatabaseConnection(); // Creates a connection to the database
+			pathHtml = path + "/" + Integer.toString(Mainclass.getPosition()) + ".html"; // Specifies
+																							// the
+																							// path
+																							// of
+																							// each
+																							// HTML
 			stmt.executeUpdate("INSERT INTO DatabaseOfURLs VALUES('" + Mainclass.getPosition() + "','" + links + "','"
-					+ pathHtml + "')"); //Inserts data into the database
-			closeDatabaseConnection(); //Closes the connection to the database
+					+ pathHtml + "')"); // Inserts data into the database
+			closeDatabaseConnection(); // Closes the connection to the database
 
 		} catch (SQLException sqle) {
 			System.err.println(sqle);
@@ -106,21 +128,24 @@ public class DatabaseConnection {
 	}
 
 	/**
-	 * Deletes all the columns from a database table.
-	 * The tbName specifies a table's name which already exists in the database.
+	 * Deletes all the columns from a database table. The tbName specifies a
+	 * table's name which already exists in the database.
 	 *
 	 * @tbName the name of a table in the database which columns will be deleted
-	 * @throws SQLException if an error occurs during the data deletion
-     */
+	 * @throws SQLException
+	 *             if an error occurs during the data deletion
+	 */
 
 	public static void deleteData(String dbName) {
 		try {
-			openDatabaseConnection(); //Creates a connection to the database
-			stmt.executeUpdate("DELETE FROM " + dbName + ";"); //Deletes data from the dbName table
-			closeDatabaseConnection(); //Closes the connection to the database
+			openDatabaseConnection(); // Creates a connection to the database
+			stmt.executeUpdate("DELETE FROM " + dbName + ";"); // Deletes data
+																// from the
+																// dbName table
+			closeDatabaseConnection(); // Closes the connection to the database
 		} catch (SQLException e) {
-							//Handles errors for JDBC
+			// Handles errors for JDBC
 			System.err.println(e);
 		}
-    }
+	}
 }
